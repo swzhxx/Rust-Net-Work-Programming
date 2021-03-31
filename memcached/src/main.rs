c738ad7656ee75238d3de0cb6273259b3c69a221
swzhxx <549909896@qq.com>
2021-03-31 22:25:06 +0800

    fix(什么都没进展， 添加了个测试 看看内存大小): 什么都没进展， 添加了个测试 看看内存大小

--- memcached/src/main.rs
+++ memcached/src/main.rs
@@ -1,12 +1,24 @@
-use std::boxed::Box;
-use std::collections::HashMap;
-use std::io::Result;
+use std::io::{self, BufRead, BufReader, Result};
 use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::str;
 use std::sync::{Arc, Mutex};
 use std::thread;
-
+use std::{boxed::Box, io::Read};
+use std::{collections::HashMap, mem};
 type Store = HashMap<Box<String>, Mutex<String>>;
 
+enum Operater {
+    set,
+    get,
+    delete,
+}
+
+#[repr(packed)]
+struct Message {
+    op: Operater,
+    key: String,
+    value: String,
+}
 struct Memcached {
     store: Store,
 }
@@ -14,11 +26,9 @@ struct Memcached {
 impl Memcached {
     pub fn new() -> Self {
         let mut store = HashMap::new();
-
         Memcached { store: store }
     }
 
-    
     pub fn get(&self, key: &Box<String>) {
         self.store.get(key);
     }
@@ -32,7 +42,15 @@ impl Memcached {
     }
 }
 
-fn handle_read(stream: TcpStream, memcahced: Arc<Memcached>) {}
+fn handle_read(mut stream: TcpStream, memcahced: Arc<Memcached>) {
+    // let len = std::mem::size_of(Message);
+    loop {
+        // let mut reader = BufReader::new(stream.try_clone().unwrap());
+        let mut buf = [0u8; 2];
+        let bytes_read = stream.read(&mut buf).unwrap();
+        println!("{}", str::from_utf8(&buf[..bytes_read]).unwrap());
+    }
+}
 
 fn main() -> io::Result<()> {
     let addr = "127.0.0.1:8080".parse::<SocketAddr>().unwrap();
@@ -40,6 +58,7 @@ fn main() -> io::Result<()> {
     let memcached = Memcached::new();
     let arc_memcached = Arc::new(memcached);
     for stream in listener.incoming() {
+        println!("incoming");
         match stream {
             Err(e) => {
                 eprintln!("{}", e);
@@ -53,4 +72,19 @@ fn main() -> io::Result<()> {
         }
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    #[test]
+    ///
+    /// test mem size of
+    fn test() {
+        let str = String::from("Hello World");
+        assert_eq!(24, std::mem::size_of_val(&str));
+        assert_eq!(1, std::mem::size_of::<Operater>());
+        assert_eq!(24, std::mem::size_of::<String>());
+        assert_eq!(49, std::mem::size_of::<Message>());
+    }
 }
